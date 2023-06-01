21049a549eb1e30c5d0ea45f77b85df860bb3188
Ram <53596726+rammolankula@users.noreply.github.com>
2023-06-01 16:44:39 +0530

    Add files via upload

---
+++ MvcCustomHandlerExcepAttributes/src/main/java/ram/learn/MvcCustomExcepAttributes.java
@@ -0,0 +1,14 @@
+package ram.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MvcCustomExcepAttributes {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcCustomExcepAttributes.class, args);
+	}
+	
+}
---
+++ MvcCustomHandlerExcepAttributes/src/main/java/ram/learn/empcontroller/Employeecontroller.java
@@ -0,0 +1,32 @@
+package ram.learn.empcontroller;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Employeecontroller {
+
+	@RequestMapping("/show")
+	public String get() {
+	    // log.info("Ram");
+		if(new Random().nextInt(10)<=5) {
+			throw new RuntimeException("Excep");
+		}
+		return "welcome";
+		
+	}
+
+	@RequestMapping("/showb")
+	public String getB(Model model) {
+	    // log.info("Ram");
+		if(new Random().nextInt(10)<=5) {
+			throw new Unavailableexception("Excep");
+		}
+		return "welcome";
+		
+	}
+}
